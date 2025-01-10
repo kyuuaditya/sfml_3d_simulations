b0863b786a7bd8eeb9ad8e733b07ceba890900c3
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-01-10 17:44:29 +0530

    added ijkluo rotation

--- main.cpp
+++ main.cpp
@@ -34,8 +34,6 @@ int main() {
     // teamporary values
     float theta = 0.0f;  // theta
     float phi = 0.0f;  // phi
-    float A = 0.0f;  // angle_A
-    float B = 0.0f;  // angle_B
 
     float x, y, z, cx, cy, cz, L, S, screenXcords, yd, screenYcords;
 
@@ -74,18 +72,41 @@ int main() {
                 if (event.key.code == sf::Keyboard::Numpad2) { // to select the play button
                     camera_z -= 0.1f;
                 }
+                // donut rotation
+                if (event.key.code == sf::Keyboard::I) { // to toggle fullscreen
+                    donut_angle_x += 0.1f;
+                }
+                if (event.key.code == sf::Keyboard::K) { // to select the play button
+                    donut_angle_x -= 0.1f;
+                }
+                if (event.key.code == sf::Keyboard::L) { // to select the play button
+                    donut_angle_y += 0.1f;
+                }
+                if (event.key.code == sf::Keyboard::J) { // to select the play button
+                    donut_angle_y -= 0.1f;
+                }
+                if (event.key.code == sf::Keyboard::U) { // to select the play button
+                    donut_angle_z += 0.1f;
+                }
+                if (event.key.code == sf::Keyboard::O) { // to select the play button
+                    donut_angle_z -= 0.1f;
+                }
             }
         }
 
 
-        float cosA = cos(A);
-        float sinA = sin(A);
-        float cosB = cos(B);
-        float sinB = sin(B);
-        for (float j = 0; j < 6.28; j += 0.015) {
+        float cosX = cos(donut_angle_x);
+        float sinX = sin(donut_angle_x);
+        float cosY = cos(donut_angle_y);
+        float sinY = sin(donut_angle_y);
+        float cosZ = cos(donut_angle_z);
+        float sinZ = sin(donut_angle_z);
+        // int count = 0;
+
+        for (float j = 0; j < 6.28; j += 0.02) {
             phi = j;
 
-            for (float i = 0; i < 6.28; i += 0.055) {
+            for (float i = 0; i < 6.28; i += 0.1) {
 
                 float cosPhi = cos(phi);
                 float sinPhi = sin(phi);
@@ -96,9 +117,14 @@ int main() {
                 float sinTheta = sin(theta);
 
                 // local cordinates of the donut
-                x = (double)(cosPhi * (donut_radius + donut_radius2 * cosTheta));
-                y = (double)(donut_radius2 * sinTheta);
-                z = (double)((sinPhi * (donut_radius + donut_radius2 * cosTheta)));
+                // x = (double)(cosPhi * (donut_radius + donut_radius2 * cosTheta));
+                x = (double)(cosZ * (cosPhi * cosY * (donut_radius2 * cosTheta + donut_radius) - sinY * (sinPhi * cosX * (donut_radius2 * cosTheta + donut_radius) - donut_radius2 * sinTheta * sinX)) + sinZ * (donut_radius2 * sinTheta * cosX + sinPhi * sinX * (donut_radius2 * cosTheta + donut_radius)));
+                // y = (double)(donut_radius2 * sinTheta);
+                y = (double)(cosZ * (donut_radius2 * sinTheta * cosX + sinPhi * sinX * (donut_radius2 * cosTheta + donut_radius)) + sinZ * (cosPhi * cosY * (donut_radius2 * cosTheta + donut_radius) - donut_radius2 * sinTheta * sinX));
+                // z = (double)((sinPhi * (donut_radius + donut_radius2 * cosTheta)));
+                z = (double)(cosPhi * sinY * (donut_radius2 * cosTheta + donut_radius) + cosY * (sinPhi * cosX * (donut_radius2 * cosTheta + donut_radius) - donut_radius2 * sinTheta * sinX));
+
+                // count++;
 
                 // adjusting local donut cordinates with reference to the camera
                 x += donut_x;
@@ -119,18 +145,19 @@ int main() {
                         sf::Vertex point;
                         point.position = sf::Vector2f(screenXcords * window.getSize().x / 4 + screen_width * window.getSize().x / 8,
                             screenYcords * window.getSize().y / 4 + screen_height * window.getSize().y / 8);
-                        point.color = sf::Color(255 * (y / (2 * (donut_radius + donut_radius2)) + 0.5),
-                            255 * (y / (2 * (donut_radius + donut_radius2)) + 0.5),
-                            255 * (y / (2 * (donut_radius + donut_radius2)) + 0.5));
+                        point.color = sf::Color(255, 255, 255);
+                        // point.color = sf::Color(255 * (y - camera_y / (2 * (donut_radius + donut_radius2)) + 0.5),
+                        //     255 * (y - camera_y / (2 * (donut_radius + donut_radius2)) + 0.5),
+                        //     255 * (y - camera_y / (2 * (donut_radius + donut_radius2)) + 0.5));
                         points.append(point);
                     }
                 }
             }
         }
-
+        // std::cout << count << std::endl;
         // howw fast the donut rotates
-        A += 0.005;
-        B += 0.003;
+        // A += 0.005;
+        // B += 0.003;
 
 
         window.clear(sf::Color::Black);
